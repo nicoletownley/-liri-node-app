00c954af3711fcaf0b38403d33ae63eec29f4f3d
Your Name <natownley@gmail.com>
2019-09-18 12:58:28 -0500

    updated read me

--- README.md
+++ README.md
@@ -4,16 +4,25 @@ Because this is a CLI App, it cannot be deployed to GitHub pages or Heroku. So I
 
 Following, is information as it relates to the application(s)
 
-What is it doing and why - 
+What is it doing and why - Liri takes in commands then disseminates information using API calls. A response is returned and with the information formatted based on user (classroom) requirements
 
 
-How the app is organized - 
+How the app is organized - The app is organized to receive commands and return data in node
 
 
-How to run the app - 
+How to run the app - You run the app by passing in one of the following argument strings, followed by a fourth argrument [3] which is the search (artist, song, movie, random-text) paramenter, respectively:
+
+node liri.js concert-this
+
+node liri.js spotify-this-song
+
+node liri.js movie-this
+
+node liri.js 
 
 
-Sreenshots, gifs or videos of the app functioning - 
+        
+Sreenshots, gifs or videos of the app functioning - ,
 
  
 A link to a deployed version of the app -
--- liri.js
+++ liri.js
@@ -46,51 +46,157 @@ function spotifyThis(lookThisUp) {
             limit: 1
         }).then(response => {
             var song = response.tracks.items[0];
-            var artArr = song.artists;
-            var artist = artArr[0].name;
-            if (artArr.length > 1) {
-                for (let i = 1; i < artArr.length; i++) {
-                    artist += " & " + artArr[i].name
+            var artistsArr = song.artists;
+            var artist = artistsArr[0].name;
+            if (artistsArr.length > 1) {
+                for (let i = 1; i < artistsArr.length; i++) {
+                    artist += " & " + artistsArr[i].name
                 }
             }
-            // spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-            //     if ( err ) {
-            //         console.log('Error occurred: ' + err);
-            //         return;
-            //     }
-
-            // Do something with 'data'
-
             var results = "\nArtist(s): " + artist + "\nSong Title: " + song.name + "\nPreview URL: " + song.preview_url + "\nAlbum: " + song.album.name
+            console.log(results);
+            logAction("spotify-this-song", lookThisUp, results);
+        }).catch(error => {
+            console.log("Could not spotify this song!\n" + error)
+        })
+}
+
+function concertThis(lookThisUp) {
+    axios.get("https://rest.bandsintown.com/artists/" + lookThisUp + "/events?app_id=codingbootcamp").then(response => {
+        var results = "\n";
+        for (obj in response.data) {
+            var formattedDate = moment(response.data[obj].datetime).format("MM/DD/YYYY");
+            if (response.data[obj].venue.region) {
+                results += "Venue Name: " + response.data[obj].venue.name + "\nVenue Location: " + response.data[obj].venue.city + ", " + response.data[obj].venue.region + ", " + response.data[obj].venue.country + "\nEvent Date: " + formattedDate + "\n\n";
+            } else {
+                results += "Venue Name: " + response.data[obj].venue.name + "\nVenue Location: " + response.data[obj].venue.city + ", " + response.data[obj].venue.country + "\nEvent Date: " + formattedDate + "\n\n";
+            }
+        }
         console.log(results);
-        logAction("spotify-this-song", lookThisUp, );
+        logAction("concert-this", lookThisUp, results);
     }).catch(error => {
-        console.log("Something went wrong\n" + error)
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data-----------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
     })
 }
 
-
-//getSpotifyTrack();
-
-function concertThis(lookThisUp) {
-                var userURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-                axios.get(userURL).then(
-                    function (response) {
-                        var jsonData = response.data;
-
-                        // for (var i = 0;  jsonData.length; i++){
-                        //     console.log(jsonData[i]);
-                        // }
-
-                        console.log(jsonData);
-
-                    })
-
-
-
+function movieThis(lookThisUp) {
+    if (!lookThisUp) {
+        lookThisUp = "Mr. Nobody";
+    }
+    axios.get("http://www.omdbapi.com/?t=" + lookThisUp + "&y=&plot=short&apikey=trilogy").then(response => {
+        var movie = response.data
+        var ratings = movie.Ratings;
+        var rtRating = "No data provided";
+        for (obj in ratings) {
+            if (ratings[obj].Source === "Rotten Tomatoes") {
+                rtRating = ratings[obj].Value;
             }
+        }
+        var results = "\nMovie Title: " + movie.Title + "\nYear Released: " + movie.Released + "\nIMDB Rating: " + movie.imdbRating + "\nRotten Tomatoes Rating: " + rtRating + "\nProduced in: " + movie.Country + "\nLanguage: " + movie.Language + "\nPlot: " + movie.Plot + "\nStarring: " + movie.Actors;
 
-            
+        console.log(results);
+        logAction("movie-this", lookThisUp, results);
+    })
+        .catch(error => {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data-----------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf-8", (error, data) => {
+        var contents = data.split(",");
+        if (error) throw console.log("Could not perform the random action!\n" + error);
+        if (contents[0] === "concert-this") {
+            concertThis(contents[1]);
+        } else if (contents[0] === "spotify-this-song") {
+            spotifyThis(contents[1]);
+        } else if (contents[0] === "movie-this") {
+            movieThis(contents[1]);
+        }
+    })
+}
+
+function logAction(inputStuff, lookThisUp, results) {
+    fs.appendFile("log.txt", inputStuff + ": " + lookThisUp + "\n" + results + "\n\n", "utf8", (err) => {
+        if (err)
+            throw console.log("The 'data to append' was not appended to the file!\n" + err);
+    })
+}
+
+searchStuff(); 
+
+// // spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+// //     if ( err ) {
+// //         console.log('Error occurred: ' + err);
+// //         return;
+// //     }
+
+// // Do something with 'data'
+
+// var results = "\nArtist(s): " + artist + "\nSong Title: " + song.name + "\nPreview URL: " + song.preview_url + "\nAlbum: " + song.album.name
+// console.log(results);
+// logAction("spotify-this-song", lookThisUp);
+//     }).catch (error => {
+//     console.log("Something went wrong\n" + error)
+// })
+// }
+
+
+// //getSpotifyTrack();
+
+// function getBands("artists") {
+//                 var userURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+//                 axios.get(userURL).then(
+//                     function (response) {
+//                         var jsonData = response.data;
+
+//                         // for (var i = 0;  jsonData.length; i++){
+//                         //     console.log(jsonData[i]);
+//                         // }
+
+//                         console.log(jsonData);
+
+//                     })
+
+
+
+//             }
+
+
 
 // getBands("Post Malone");
